53e37457853ef8984598c592adedee1406450d85
RichyWalter <richardwalter@web.de>
2017-11-09 17:47:20 +0100

    Scoreboard, winnig, clearArea

--- pong_ardu.ino
+++ pong_ardu.ino
@@ -9,22 +9,44 @@ int potPinP1ay1 = 2;      //input pin for player 1s potentiometer
 int potPinP1ay2 = 3;      //input pin for player 2s potentiometer
 int potVal1 = 0;          //store the value of player 1s poti
 int potVal2 = 0;          //store the value of player 2s poti
-bool gamearea[16][16]
+boolean gamearea[16][16];
 
 void setup() {
-  for(int horiz = 0, horiz <= 8, horiz++){      //horizontral iterrieren
-      for(int vert =0, vert <= 8, vert++){
+  for(int horiz = 0; horiz <= 8; horiz++){      //horizontral iterrieren
+      for(int vert =0; vert <= 8; vert++){
         gamearea[horiz][vert]= false;
       }
-
   }
-
 }
 
 void clearColumn(int column){
-  for(i=3, i<=15, i++){
+  for(int i=3; i<=15; i++){
     gamearea[i][column]= false;
   }
+}
+
+//clear the matrix, 1 for complete Matrix / 2 for only Playground
+void clearGamearea(int area){
+  int i;
+  switch(area){
+    case 1: i=0;                
+    case 2: i=3; 
+  }
+  for(int i=0; i<=15; i++){
+    for(int j=0; j<=15; j++){
+      gamearea[i][j]=false;
+    }
+  }
+}
+
+
+void winner(int Player){
+  clearGamearea(2);  
+  switch(Player){
+      case 1: gamearea[15][0];
+      case 2: gamearea[15][15];
+  }
+  
 }
 
 void potiControl(int value ,int column){
@@ -96,6 +118,28 @@ void potiControl(int value ,int column){
   }
 }
 
+void AddPointScoreboard(int Player){
+  if(Player == 1){
+    for(int i=0; i<=6; i++){
+      if(!(gamearea[0][i])){
+         gamearea[0][i] = true;
+      }
+      if(gamearea[0][6]){
+        winner(1);
+      }else{
+        for(int i=15; i>=9; i--){
+            if(!(gamearea[0][i])){
+              gamearea[0][i] = true;
+             }
+            if(gamearea[0][6]){
+            winner(2);
+        
+      }
+    }
+   } 
+  }
+  }
+}
 
 
 void loop() {
@@ -105,9 +149,5 @@ void loop() {
   potVal2 = analogRead(potPinP1ay2);
   potiControl(potVal1, 1);
   potiControl(potVal2, 2);
-
-
-
-
 
   }
